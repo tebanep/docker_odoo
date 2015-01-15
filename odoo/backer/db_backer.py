89f73f29537aa642716c05c5eff09c115a00827c
Esteban Echeverry <tebanep@gmail.com>
2015-01-15 17:08:31 -0500

    Add database and mode options to backer argsparse

--- odoo/backer/db_backer.py
+++ odoo/backer/db_backer.py
@@ -4,9 +4,11 @@ import subprocess
 from datetime import datetime
 
 
-def backup_postgresql(database, backup_path, host="localhost", port="5432",
-                      user="postgres", password="postgres"):
-
+def backup_postgresql(dir, host="localhost", port="5432",
+                      database="postgres", user="postgres", password="postgres"):
     now = datetime.now()
+    backup_path = dir + "/" + "{}_{}_{}".format(database,
+                                                "back",
+                                                now.isoformat().split(".")[0])
     cmd = ['pg_dump', '-U', user, '-Fc', database, '-f', backup_path]
     subprocess.call(cmd, stdout=subprocess.PIPE)
