4cc9c8a1b7018b92d45c05a57e6d96b77ed6df19
Esteban Echeverry <tebanep@gmail.com>
2015-01-15 16:07:35 -0500

    Set up entry argparse for backer program

--- odoo/backer/db_backer.py
+++ odoo/backer/db_backer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
 import subprocess
+from datetime import datetime
 
 
-def run_cmd(cmd):
-    subprocess.call(cmd)
+def backup_postgresql(database, backup_path, host="localhost", port="5432",
+                      user="postgres", password="postgres"):
+
+    now = datetime.now()
+    cmd = ['pg_dump', '-U', user, '-Fc', database, '-f', backup_path]
+    subprocess.call(cmd, stdout=subprocess.PIPE)
