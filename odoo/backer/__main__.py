e7cb8fec13eba01b6d2b51f9b31cd451223f9bc8
Esteban Echeverry <tebanep@gmail.com>
2015-01-16 02:40:37 -0500

    Add crontab with /backer command to regularly backup postgresql's database

---
+++ odoo/backer/__main__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import argparse
+# from . import vol_backer
+import db_backer
+
+
+def main():
+    mode_dict = {"database": db_backer.backup_postgresql,
+                 "volume": [],
+                 "all": []}
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", help="Define backup directory")
+    parser.add_argument("-d", "--database", help="PostgreSQL database")
+    parser.add_argument("-m", "--mode",
+                        choices=mode_dict.keys(),
+                        default="database",
+                        help="Define backup mode")
+
+    args = parser.parse_args()
+    dir = args.dir
+    mode = args.mode
+    database = args.database
+
+    if mode == "database":
+        print "database mode!"
+        option_dict = {'dir': dir}
+        if database:
+            option_dict['database'] = database
+        print database, option_dict
+        cmd = mode_dict[mode]
+        cmd(**option_dict)
+    elif mode == "volume":
+        print "volume mode!"
+
+
+if __name__ == '__main__':
+    main()
--- odoo/backer/db_backer.py
+++ odoo/backer/db_backer.py
@@ -8,7 +8,7 @@ def backup_postgresql(dir, host="localhost", port="5432",
                       database="postgres", user="postgres", password="postgres"):
     now = datetime.now()
     backup_path = dir + "/" + "{}_{}_{}".format(database,
-                                                "back",
+                                                "db",
                                                 now.isoformat().split(".")[0])
     cmd = ['pg_dump', '-U', user, '-Fc', database, '-f', backup_path]
     subprocess.call(cmd, stdout=subprocess.PIPE)
